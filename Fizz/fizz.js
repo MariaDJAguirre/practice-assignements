02f05e4e2a5b283e83a15cb6efbab774bd8438d0
Maria Aguirre <mercy27.ma@gmail.com>
2023-01-28 12:17:49 -0800

    commit

---
+++ Fizz/fizz.js
@@ -0,0 +1,9 @@
+for(var i=1; 1<=101; i++){
+    if(i % 3===0){
+
+console.log("Fizz")
+}
+        if(i % 5===0){
+        console.log("Buzz")
+}
+console.log(i)}
---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+function displayName(elementName) {
+    console.log(elementName);
+}
+
+function clicked(element){
+    console.log("Ive been clicked");
+    element.innerText="Goodbye";
+}
